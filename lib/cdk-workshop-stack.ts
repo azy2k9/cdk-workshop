663dace939c84032400414aa9efa2acbbedeaffa
azy2k9 <azy2k9@gmail.com>
2023-01-20 18:46:03 +0000

    Switch out to using nodejs based lambda function to avoid tsc before deploying

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,16 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // defines an AWS Lambda resource
-    const helloLambda = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X, // use the node 14 runtime
-      code: lambda.Code.fromAsset('lambda'), //code loaded from "lambda" directory
-      handler: 'hello.handler', //file is "hello", function is "handler"
+    // defines an AWS nodejs function
+    new nodejs.NodejsFunction(this, "HelloHandler", {
+      entry: './lambda/hello.ts',
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_16_X, // use the node 16 runtime
     });
   }
 }
