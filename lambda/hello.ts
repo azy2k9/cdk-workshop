8ee662b6075e0bc36c2de9b47eacc478d2e5af22
azy2k9 <azy2k9@gmail.com>
2023-01-25 13:02:33 +0000

    Add an api gateway, lambda and a dynamodb

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,8 +1,39 @@
+import { DynamoDB } from 'aws-sdk';
+
 exports.handler = async function (event: any) {
-  console.log('request:', JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, CDK! You've hit ${event.path}\n`
+  if (process.env.DYNAMODB === '' || !process.env.DYNAMODB) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: {
+        error: 'Cannot find DynamoDB table name'
+      }
+    };
+  }
+
+  const query = {
+    TableName: process.env.DYNAMODB
   };
+
+  try {
+    const documentClient = new DynamoDB.DocumentClient();
+    const results = await documentClient.scan(query).promise();
+    console.log(results);
+    return results;
+  } catch (error) {
+    console.log('hit an error');
+    console.log('hit an error');
+    console.log('hit an error');
+    console.log(error);
+    console.log('hit an error');
+    console.log('hit an error');
+    console.log('hit an error');
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: {
+        error: 'Error finding table results'
+      }
+    };
+  }
 };
